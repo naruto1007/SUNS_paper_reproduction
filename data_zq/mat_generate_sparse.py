a542dc6f3ce84da425913e24bf4688a0ad5400cd
naruto1007 <naruto19900824@gmail.com>
2024-07-19 17:37:31 +0800

    update mat generate GT test

--- data_zq/mat_generate_sparse.py
+++ data_zq/mat_generate_sparse.py
@@ -6,6 +6,26 @@ from scipy import sparse
 from scipy.io import savemat, loadmat
 
 
+def mat_generate_sparse_GT_original(dir_Masks):
+    dir_all = glob.glob(os.path.join(dir_Masks, '*FinalMasks*.mat'))
+    for path_name in dir_all:
+        file_name = os.path.split(path_name)[1]
+        if '_sparse' not in file_name:
+            print(file_name)
+            try:  # If file_name is saved in '-v7.3' format
+                mat = h5py.File(path_name, 'r')
+                FinalMasks = np.array(mat['FinalMasks']).astype('bool')
+                mat.close()
+            except OSError:  # If file_name is not saved in '-v7.3' format
+                mat = loadmat(path_name)
+                FinalMasks = np.array(mat["FinalMasks"]).transpose([2, 1, 0]).astype('bool')
+
+            (ncells, Ly, Lx) = FinalMasks.shape
+            GTMasks_2 = sparse.coo_matrix(FinalMasks.reshape(ncells, Lx * Ly).T)
+            savemat(os.path.join(path_name[:-4] + '_sparse_original.mat'), \
+                    {'GTMasks_2': GTMasks_2}, do_compression=True)
+
+
 def mat_generate_sparse_GT(dir_Masks):
     dir_all = glob.glob(os.path.join(dir_Masks, '*FinalMasks*.mat'))
     for path_name in dir_all:
@@ -22,7 +42,7 @@ def mat_generate_sparse_GT(dir_Masks):
 
             (Lx, Ly, ncells) = FinalMasks.shape
             GTMasks_2 = sparse.coo_matrix(FinalMasks.reshape(ncells, Lx * Ly).T)
-            savemat(os.path.join(path_name[:-4] + '_sparse_new.mat'), \
+            savemat(os.path.join(path_name[:-4] + '_sparse_generated.mat'), \
                     {'GTMasks_2': GTMasks_2}, do_compression=True)
 
 
@@ -48,8 +68,8 @@ def mat_generate_sparse_output(dir_Masks):
 
 dir_Masks_GT = "D:\PyCharm_project\SUNS_paper_reproduction\data_zq\CalmAn_dataset\YST\GT Masks"
 # dir_Masks_GT = "D:\PyCharm_project\SUNS_paper_reproduction\data_zq\\Neurofinder\\train\GT Masks"
-mat_generate_sparse_GT(dir_Masks_GT)
-
-dir_Masks_output = "D:\PyCharm_project\SUNS_paper_reproduction\data_zq\output masks all methods_copied\CaImAn dataset\YST\SUNS noSF output_masks"
-# dir_Masks_output = "D:\PyCharm_project\SUNS_paper_reproduction\data_zq\output masks all methods_copied\\Neurofinder\\train videos\SUNS noSF output_masks"
-mat_generate_sparse_output(dir_Masks_output)
+mat_generate_sparse_GT_original(dir_Masks_GT)
+#
+# dir_Masks_output = "D:\PyCharm_project\SUNS_paper_reproduction\data_zq\output masks all methods_copied\CaImAn dataset\YST\SUNS noSF output_masks"
+# # dir_Masks_output = "D:\PyCharm_project\SUNS_paper_reproduction\data_zq\output masks all methods_copied\\Neurofinder\\train videos\SUNS noSF output_masks"
+# mat_generate_sparse_output(dir_Masks_output)
