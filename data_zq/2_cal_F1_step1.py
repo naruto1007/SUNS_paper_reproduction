a8cf8da8603fcc9e655e8979a6780d5a8f4438b6
naruto1007 <naruto19900824@gmail.com>
2024-07-23 17:33:48 +0800

    update cal

---
+++ data_zq/2_cal_F1_step1.py
@@ -0,0 +1,50 @@
+# calculate all metrics for every kind of data, saved in Output_Info_All.mat
+# the mat file includes list_Recall, list_Precision, list_F1, list_time,list_time_frame
+# a list_F1 includes n scores, n=6 or n=10 maybe
+import numpy as np
+import os
+from scipy.io import savemat, loadmat
+from suns.PostProcessing.evaluate import GetPerformance_Jaccard_2
+
+list_Exp_ID = ['YST_part11', 'YST_part12', 'YST_part21', 'YST_part22']
+nvideo = len(list_Exp_ID)
+list_CV = list(range(0, nvideo))
+num_CV = len(list_CV)
+list_Recall = np.zeros((num_CV, 1))
+list_Precision = np.zeros((num_CV, 1))
+list_F1 = np.zeros((num_CV, 1))
+list_time = np.zeros((num_CV, 4))
+list_time_frame = np.zeros((num_CV, 4))
+
+dir_Masks_GT = "D:\PyCharm_project\SUNS_paper_reproduction\data_zq\CalmAn_dataset\YST\GT Masks"
+dir_Masks_output = "D:\PyCharm_project\SUNS_paper_reproduction\data_zq\output masks all methods_copied\CaImAn dataset\YST\SUNS noSF output_masks"
+dir_GTMasks = os.path.join(dir_Masks_GT, 'FinalMasks_')
+dir_OutputMasks = os.path.join(dir_Masks_output, 'Output_Masks_')
+
+time_total = 0
+time_frame = 0
+
+for CV in list_CV:
+    Exp_ID = list_Exp_ID[CV]
+    print('Video ', Exp_ID)
+    # GT_sparse
+    filename_GT = dir_GTMasks + Exp_ID + '_sparse.mat'
+    data_GT = loadmat(filename_GT)
+    GTMasks_2 = data_GT['GTMasks_2'].transpose()
+    # Mask_sparse
+    filename_output = dir_OutputMasks + Exp_ID + '_sparse.mat'
+    data_Masks = loadmat(filename_output)
+    Masks_2 = data_Masks['Masks'].transpose()
+    # calculate
+    (Recall, Precision, F1) = GetPerformance_Jaccard_2(GTMasks_2, Masks_2, ThreshJ=0.5)
+    print({'Recall': Recall, 'Precision': Precision, 'F1': F1})
+
+    list_Recall[CV] = Recall
+    list_Precision[CV] = Precision
+    list_F1[CV] = F1
+    list_time[CV] = time_total
+    list_time_frame[CV] = time_frame
+
+    Info_dict = {'list_Recall': list_Recall, 'list_Precision': list_Precision, 'list_F1': list_F1,
+                 'list_time': list_time, 'list_time_frame': list_time_frame}
+    savemat(os.path.join(dir_Masks_output, 'Output_Info_All_cal.mat'), Info_dict)
---
+++ data_zq/2_cal_F1_step2.py
@@ -0,0 +1,12 @@
+# from Output_Info_All.mat for every kind of data
+# the mat file includes list_Recall, list_Precision, list_F1, list_time,list_time_frame
+# a list_F1 includes n scores, n=6 or n=10 maybe
+
+# from list_Recall, list_Precision, list_F1, we can get a mean value and a std value (error bar), n=6 or n=10
+# we get 4 methods results with a mean and a std value for every method
+
+# legend: method name
+# x: Recall Precision F1
+# data:
+
+
