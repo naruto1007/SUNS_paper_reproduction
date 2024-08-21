dba269a82c1a76f8b13b88f834c6a65686f995b1
naruto1007 <naruto19900824@gmail.com>
2024-08-21 17:51:40 +0800

    update part 2 fig plot for abo 275 to 175

--- data_zq/output masks all methods_copied/ABO/175/2_cal_F1_step2.py
+++ data_zq/output masks all methods_copied/ABO/175/2_cal_F1_step2.py
@@ -8,9 +8,37 @@
 # legend: method name
 # x: Recall Precision F1
 # data: calculated mean value and std, for R, P, F1
-
+import os
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.io import loadmat
+
+x_labels = ['Recall', 'Precision', 'F1']
+y_labels = ['SUNS', 'STNeuroNet', 'CaImAn Batch', 'Suite2p']
+file_names = ['SUNS noSF output_masks', 'STNeuroNet FinalMasks', 'CaImAn Batch Masks', 'Suite2p Masks']
+
+Recall_tmp = []
+Precision_tmp = []
+F1_tmp = []
+
+for i in range(len(file_names)):
+    res_file = os.path.join(".", file_names[i], "Output_Info_All_cal.mat")
+    mat = loadmat(res_file)
+    list_Recall = np.array(mat["list_Recall"])
+    list_Precision = np.array(mat["list_Precision"])
+    list_F1 = np.array(mat["list_F1"])
+    list_time = np.array(mat["list_time"])
+    list_time_frame = np.array(mat["list_time_frame"])
+    Recall_tmp.append(np.squeeze(list_Recall))
+    Precision_tmp.append(np.squeeze(list_Precision))
+    F1_tmp.append(np.squeeze(list_F1))
+
+# [4, 10] ndarray, 4 methods, 10 data points
+Recall = np.array(Recall_tmp)
+Precision = np.array(Precision_tmp)
+F1 = np.array(F1_tmp)
+
+
 
 # 假设我们有以下四组数据
 recall = [0.8, 0.7, 0.9, 0.6]
@@ -18,24 +46,21 @@ precision = [0.7, 0.6, 0.8, 0.5]
 f1 = [0.75, 0.65, 0.85, 0.55]
 mean_std = [(0.75, 0.05), (0.65, 0.04), (0.85, 0.03), (0.55, 0.02)]
 
-# 设置横坐标标签
-x_labels = ['Recall', 'Precision', 'F1']
 x = np.arange(len(x_labels))
 
 # 设置纵坐标范围
 y_min = min([min(m - s, m + s) for m, s in mean_std])
 y_max = max([max(m - s, m + s) for m, s in mean_std])
 y_range = y_max - y_min
-y_labels = ['SUNS', 'STNeuroNet', 'CaImAn Batch', 'Suite2p']
 
 # 绘制柱状图
 fig, ax = plt.subplots()
-width = 0.2
+width = 0.18
 for i in range(len(recall)):
-    ax.bar(x - width + i * width, [recall[i], precision[i], f1[i]], width, label=y_labels[i])
+    ax.bar(x - width + i * width, [recall[i], precision[i], f1[i]], width, align='center', label=y_labels[i])
     ax.errorbar(x - width + i * width, [recall[i], precision[i], f1[i]],
-                yerr=[mean_std[i][1], mean_std[i][1], mean_std[i][1]], fmt='o', capsize=5)
-
+                yerr=[mean_std[i][1], mean_std[i][1], mean_std[i][1]], fmt=',', capsize=5, color='black')
+    # ax.scatter(x - width + i * width, )
 # 设置横坐标标签和刻度
 fontsize = 14
 ax.set_xticks(x)
@@ -55,6 +80,5 @@ ax.tick_params(direction='in')
 ax.set_title('ABO 275 μm to 175 μm', fontsize=fontsize)
 
 # 保存或显示图形
-plt.savefig("2_cal_F1_step2.png")
+# plt.savefig("2_cal_F1_step2.png")
 plt.show()
-
