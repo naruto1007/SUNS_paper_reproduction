e1b190bfc6c4775ed6e52c51d599eed6da5ee02b
naruto1007 <naruto19900824@gmail.com>
2024-08-28 17:27:24 +0800

    update neurofinder fig plot
    01.00, 01.01 correct, others res = 0

--- data_zq/output masks all methods_copied/Neurofinder/train/SUNS online noSF output_masks/0_mat_visualization.py
+++ data_zq/output masks all methods_copied/Neurofinder/train/SUNS online noSF output_masks/0_mat_visualization.py
@@ -18,11 +18,11 @@ def mat_visualization_GT(filename):
 def mat_visualization_output(filename):
     try:
         mat = h5py.File(filename, 'r')
-        Masks = np.array(mat['Masks'])
+        Masks = np.array(mat['Masks']).transpose([2, 1, 0])
         mat.close()
     except OSError:
         mat = loadmat(filename)
-        Masks = np.array(mat["Masks"]).transpose([1, 2, 0])
+        Masks = np.array(mat["Masks"]).transpose([2, 1, 0])
     print("Masks.shape: ", Masks.shape)
     data_output = Masks[:, :, 3]
     coordinates = np.where(data_output == 1)
@@ -31,12 +31,12 @@ def mat_visualization_output(filename):
 
 
 dir_Masks_GT = "D:\PyCharm_project\SUNS_paper_reproduction\data_zq\\Neurofinder\\train\GT Masks"
-filename_GT = os.path.join(dir_Masks_GT, "FinalMasks_01.00.mat")
+filename_GT = os.path.join(dir_Masks_GT, "FinalMasks_02.00.mat")
 mat_visualization_GT(filename_GT)
 
 # dir_Masks_output = "D:\PyCharm_project\SUNS_paper_reproduction\data_zq\output masks all methods_copied\CaImAn dataset\YST\SUNS noSF output_masks"
 # filename_output = os.path.join(dir_Masks_output, "Output_Masks_YST_part11.mat")
 
-filename_output = "Output_Masks_01.00.mat"
+filename_output = "Output_Masks_02.00.mat"
 
 mat_visualization_output(filename_output)
